33a39d9a8a52e35d546651f722bfcf86a97491bd
alexismengual <lexmengual@gmail.com>
2020-03-05 18:38:40 +0100

    Select SQL in C#

---
+++ SqlCodeInCSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using SqlCodeInCSharp.Properties;
+
+namespace SqlCodeInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(Resources.sqlConnection);
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(Resources.sqlQuery, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["StudentId"] + ", "
+                        + reader["Name"] + ", " + reader["Surname"] + ", "
+                        + reader["Birthday"]);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
